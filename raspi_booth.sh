a7104d19edfb45ce4b4a6dc37a0d7611382e31f3
satare <aymeric.defossez@gmail.com>
2017-10-29 18:00:41 +0100

    Update raspi_booth.sh

--- raspi_booth.sh
+++ raspi_booth.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #param :
 photoDir="/home/pi/Photos/"
+photoGPIO=3
+resetGPIO=4
 #end params
 
 
 #launch python script for reboot
-python shut_and_reboot.py 4 & # arg : gpio pin to trigger reboot
+python shut_and_reboot.py $resetGPIO & # arg : gpio pin to trigger reboot
 
 #create photoDir
 if [ ! -d "$photoDir" ]; then
@@ -15,4 +17,4 @@ fi
 #first usbKey is /dev/sda1 (first partition)
 sudo mount /dev/sda1 $photoDir -o uid=pi,gid=pi
 #launch photobooth script
-python photo.py $photoDir 3 & # arg1 : photoDir, arg2 : gpioPin to trigger
+python photo.py $photoDir $photoGPIO & # arg1 : photoDir, arg2 : gpioPin to trigger
